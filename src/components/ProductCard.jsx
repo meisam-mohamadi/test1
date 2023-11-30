0944824b4b454e90cb3985e7accbf562f8bb07fc
meysam mohamadi <mohamadi.meisam@hotmail.com>
2023-11-30 16:12:10 +0330

    salam

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProductCard = (props) => {
+  console.log(product);
+  return (
+    <div onClick={alert("hahahahah")}>
+      <h1 className="product_name">{product.name}</h1>
+      <h2>{product.price}</h2>
+    </div>
+  );
+};
+
+export default ProductCard;
