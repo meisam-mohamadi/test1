1bfa7f7b65e987f58a5b6ea9d62d744f165469f4
meysam mohamadi <mohamadi.meisam@hotmail.com>
2023-11-30 16:17:45 +0330

    salam2

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ProductCard = (props) => {
   console.log(product);
   return (
-    <div onClick={alert("hahahahah")}>
+    <div onClick={alert("hohohohoho")}>
       <h1 className="product_name">{product.name}</h1>
       <h2>{product.price}</h2>
     </div>
